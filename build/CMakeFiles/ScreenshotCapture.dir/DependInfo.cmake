67e4c724b2255423ab0df3413ed86ee053c6689d
Sergey Sitnikov <821654@mail.ru>
2025-05-07 12:17:23 +0300

    графический интерфейс

---
+++ build/CMakeFiles/ScreenshotCapture.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ssv/code/ScreenCard/build/ScreenshotCapture_autogen/mocs_compilation.cpp" "CMakeFiles/ScreenshotCapture.dir/ScreenshotCapture_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/ScreenshotCapture.dir/ScreenshotCapture_autogen/mocs_compilation.cpp.o.d"
+  "/home/ssv/code/ScreenCard/main.cpp" "CMakeFiles/ScreenshotCapture.dir/main.cpp.o" "gcc" "CMakeFiles/ScreenshotCapture.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ScreenshotCapture.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ScreenshotCapture.
+# This may be replaced when dependencies are built.
